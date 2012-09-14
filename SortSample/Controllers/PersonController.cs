00eb8af967ac91dc838b82d9f41e029497636d52
Arjun Shetty <arjunshetty2020@gmail.com>
2012-09-14 23:13:49 +0530

    first commit

---
+++ SortSample/Controllers/PersonController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SortSample.Models;
+using SortSample.Models.Util;
+using PagedList;
+using SortSample.Properties;
+using System.Xml.Linq;
+using System.IO;
+
+namespace SortSample.Controllers
+{
+    public class PersonController : Controller
+    {
+        private static int _defaultPageSize = Settings.Default.DefaultPageSize;//gets the configurable page size from the web.config file
+
+        static List<Person> _persons;
+
+        /// <summary>
+        /// CONSTRUCTOR : 
+        /// fill person list from the xml file in the App_Data folder
+        /// </summary>
+        public PersonController()
+        {
+            var doc = XDocument.Load(Path.Combine(AppDomain.CurrentDomain.GetData("DataDirectory").ToString(), "sampledata.xml"));
+            _persons = (from n in doc.Descendants("record") select new Person() { FirstName = n.Element("FirstName").Value, LastName = n.Element("LastName").Value }).ToList();
+        }
+
+        //
+        // GET: /Person/
+
+        /// <summary>
+        /// Index view for Person
+        /// </summary>
+        /// <param name="q">Search string</param>
+        /// <param name="s">Name of thr property by which the list is to be sorted</param>
+        /// <param name="p">Page number</param>
+        /// <returns>A view with paged list of Person object</returns>
+        /// 
+        public ActionResult Index(string q = "", string s = "FirstName", int p = 1)
+        {
+            var pl = _persons.Search<Person>(q).Sort<Person>(s).ToPagedList(p, _defaultPageSize);
+
+            return View(pl);
+        }
+
+    }
+}
---
+++ SortSample/Models/Util/Util.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SortSample.Models.Util
+{
+    public static class Extenstion
+    {
+        public static IEnumerable<T> Sort<T>(this IEnumerable<T> lst, string sortString) where T : class
+        {
+            if (sortString.Split(' ').Count() > 1 && sortString.Split(' ')[1].EndsWith("Desc", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return lst.OrderByDescending(p => p.GetType().GetProperty(sortString.Split(' ')[0]).GetValue(p, null));
+            }
+            else
+            {
+                return lst.OrderBy(p => p.GetType().GetProperty(sortString).GetValue(p, null));
+            }
+        }
+
+        public static IEnumerable<T> Search<T>(this IEnumerable<T> lst, string searchString) where T : class
+        {
+            var p = lst.Where(i => ComplexTypeContains<T>(i, searchString));
+            return p;
+        }
+
+        private static bool ComplexTypeContains<T>(T complexObj, string searchString)
+        {
+            foreach (var pi in complexObj.GetType().GetProperties())
+            {
+                if (pi.GetValue(complexObj, null).ToString().IndexOf(searchString.ToString(), StringComparison.InvariantCultureIgnoreCase) >= 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class DataGen
+    {
+        private Random GetRandomizer()
+        {
+            System.Threading.Thread.Sleep(1);
+            return new Random(DateTime.Now.Millisecond);
+        }
+
+        public string GenerateFirstName()
+        {
+            string[] fNames = { "Arjun", "Larry", "Seregy", "Michael", "Albert", "Aishwarya", "Priyanka", "Genilia", "James", "Abdul", "Goerge", "Elaine", "Joey", "Amir", "Penelope", "Rani", "Anna", "Mark", "Jon", "Morgan" };
+
+            return GetRandomValueFromStringArray(fNames);
+        }
+
+        public string GenerateLastName()
+        {
+            string[] lNames = { "Brin", "Page", "Shetty", "Jackson", "Einstein", "Rai", "Chopra", "D'Souza", "Bond", "Taylor",
+            "Costanza", "Benes", "Tribbiani", "Khan", "Cruz", "Mukherjee", "Hazare", "Skeet", "Freeman", "Kalam"};
+
+            return GetRandomValueFromStringArray(lNames);
+        }
+
+        private string GetRandomValueFromStringArray(string[] sArray)
+        {
+            Random r = GetRandomizer();
+
+            return sArray[r.Next(sArray.Length)];
+        }
+    }
+}
